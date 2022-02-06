d57692d28d0adabc917588dd95dceb2ab546e5a2
root <root@ip-172-31-33-50.us-east-2.compute.internal>
2022-02-06 17:24:24 +0000

    done

---
+++ youtube/src/test/java/youtube/AppTest.java
@@ -0,0 +1,20 @@
+package youtube;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
